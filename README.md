1736c8397ff64d5216c5ac455bdffafbafec03ee
Iurii <bb.yoshka@gmail.com>
2014-11-28 01:14:49 +0200

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+so wow doge
+by юрачка
+avaible in the chrome store as an extension 
+https://chrome.google.com/webstore/detail/so-wow-doge/jgedbgomfaafnkbpbcbejigflkfladig?authuser=0
---
+++ background.js
@@ -0,0 +1,9 @@
+$('body').on('mouseup', 'div.post_like, div.like_wrap, a.UFILikeLink',  function() {
+	 
+	if($("#dog").length===0){
+		$("body").append('<img src="'+  chrome.extension.getURL("doge.png") +'" id="dog" style="display: none; position: fixed; right: 0px; bottom: 0px; z-index:9999;" />');
+		}
+	$("#dog").animate({ height: "toggle" }, 100).delay(250).animate({ height: "toggle" }, 100);	
+ });
+
+ 
